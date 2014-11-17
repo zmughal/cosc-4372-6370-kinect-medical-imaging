822517d99bc370e1aa6aa2df8d4409b09a360dc2
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-11-17 15:06:49 -0800

    add hand extension gesture
    The hand position relative to the x-position of the neck is used to
    calculate the rotation speed linearly between a minimum and maximum
    rotation speed.
    
    closes #3 <https://github.com/zmughal/cosc-4372-6370-kinect-medical-imaging/issues/3>.

--- CaptainCharlie/Assets/Scripts/Kinect/GestureBuilderDetector.cs
+++ CaptainCharlie/Assets/Scripts/Kinect/GestureBuilderDetector.cs
@@ -7,34 +7,68 @@ public class GestureBuilderDetector : MonoBehaviour
 	public GameObject Kinect;
 	public float rotationSpeed = 20.0f;
 	public GameObject objectToManipulate;
+
+	public GUIText leftDebugString;
+	public GUIText rightDebugString;
 	
 	public Vector3 lastRHCoordinate;
 	public Vector3 lastLHCoordinate;
 
+	public float xExtensionMin = 0.30f;
+	public float xExtensionMax = 0.70f;
+	public float maxRotationSpeed = 40.0f;
+	public float minRotationSpeed = 0.0f;
+
+	private float rHExtensionFactor;
+	private float lHExtensionFactor;
+
 	// Update is called once per frame
 	void Update ()
 	{
+		HandExtensionGestureUpdate ();
+	}
+
+	/* this uses the distance from the torso to determine the factor of rotation */
+	void HandExtensionGestureUpdate() {
 		KinectController kinect = Kinect.GetComponent<KinectController>();
 		
 		Vector3 rHPosition = kinect.PlayerSkeleton.GetPosition(JointType.HandRight);
 		Vector3 lHPosition = kinect.PlayerSkeleton.GetPosition(JointType.HandLeft);
+		Vector3 neckPosition = kinect.PlayerSkeleton.GetPosition(JointType.Neck);
+		
+		rHExtensionFactor = System.Math.Abs(rHPosition.x - neckPosition.x);
+		lHExtensionFactor = System.Math.Abs(lHPosition.x - neckPosition.x);
+		rightDebugString.text = rHExtensionFactor.ToString();
+		leftDebugString.text  = lHExtensionFactor.ToString();
 
+		rightHandMovingRight ();
+		leftHandMovingLeft ();
+	}
+
+	/* this calculates the direction of a hand movement between frames to determine actions */ 
+	void HandMovementGestureUpdate() {
+		KinectController kinect = Kinect.GetComponent<KinectController>();
+		
+		Vector3 rHPosition = kinect.PlayerSkeleton.GetPosition(JointType.HandRight);
+		Vector3 lHPosition = kinect.PlayerSkeleton.GetPosition(JointType.HandLeft);
+		Vector3 neckPosition = kinect.PlayerSkeleton.GetPosition(JointType.Neck);
 
 		// Which direction has the joint moved since the last time?
 		Vector3 rHDelta = rHPosition - lastRHCoordinate;
 		Vector3 lHDelta = lHPosition - lastLHCoordinate;
-
+		
 		// Set the new value of last coordinate to the current position
 		lastRHCoordinate = rHPosition;
 		lastLHCoordinate = lHPosition;
 
+
 		// will get a 0/0 error if mouse does not move
 		Vector3 rdirection = rHDelta.normalized;
 		Vector3 ldirection = lHDelta.normalized;
-
+		
 		float rdot = Vector3.Dot (rdirection, Vector3.up);
 		float ldot = Vector3.Dot (ldirection, Vector3.up);
-
+		
 		if (rdot > 0.5) { //can be >= for sideways
 			//UP
 			Debug.Log ("Moving up 2");
@@ -55,7 +89,7 @@ public class GestureBuilderDetector : MonoBehaviour
 				//LEFT
 			}
 		}
-
+		
 		if (ldot > 0.5) { //can be >= for sideways
 			//UP
 			Debug.Log ("Moving up 2");
@@ -77,13 +111,26 @@ public class GestureBuilderDetector : MonoBehaviour
 				//LEFT
 			}
 		}
+	}
 
+	/* If the hands are less than a range, then they are in a deadzone, that is, they do no actions */
+	bool isInDeadzone( float xExtension ) {
+		return xExtension <= xExtensionMin;
+	}
 
+	float calculateRotationSpeed( float xExtension ) {
+		float clippedExtension = System.Math.Min (xExtension, xExtensionMax) - xExtensionMin;
+		float slope = (maxRotationSpeed - minRotationSpeed) / (xExtensionMax - xExtensionMin);
+		float intercept = minRotationSpeed;
+		return slope * clippedExtension + intercept;
 	}
 
 	void rightHandMovingRight() {
 		/* rotate right */
-		objectToManipulate.transform.Rotate(Vector3.up, -(Mathf.PI * Time.deltaTime * rotationSpeed));
+		if (! isInDeadzone (rHExtensionFactor)) {
+			float rotSpeed = calculateRotationSpeed(rHExtensionFactor);
+			objectToManipulate.transform.Rotate(Vector3.up, -(Mathf.PI * Time.deltaTime * rotSpeed));
+		}
 	}
 
 	void rightHandMovingLeft() {
@@ -104,7 +151,10 @@ public class GestureBuilderDetector : MonoBehaviour
 
 	void leftHandMovingLeft() {
 		/* rotate left */
-		objectToManipulate.transform.Rotate(Vector3.up, Mathf.PI * Time.deltaTime * rotationSpeed);
+		if (! isInDeadzone ( lHExtensionFactor)) {
+				float rotSpeed = calculateRotationSpeed( lHExtensionFactor );
+				objectToManipulate.transform.Rotate (Vector3.up, Mathf.PI * Time.deltaTime * rotSpeed);
+		}
 	}
 
 	void leftHandMovingUp() {
